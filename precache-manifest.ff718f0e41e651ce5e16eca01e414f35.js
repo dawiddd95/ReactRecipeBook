dc98aea1f0e26d24b3c835794b9d5bd1b0555d72
Dawiddd95 <dawlyc@wp.pl>
2019-04-07 22:28:54 +0200

    Updates

---
+++ precache-manifest.ff718f0e41e651ce5e16eca01e414f35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactRecipeBook/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7884afb693bc74ea12e4",
+    "url": "/ReactRecipeBook/static/js/runtime~main.b22db41e.js"
+  },
+  {
+    "revision": "fbdb0b6221e7471bba5a",
+    "url": "/ReactRecipeBook/static/js/main.2cfec6cb.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/ReactRecipeBook/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "fbdb0b6221e7471bba5a",
+    "url": "/ReactRecipeBook/static/css/main.4fecb1c2.chunk.css"
+  },
+  {
+    "revision": "1b18b6fa1d301f87de70c4d79f9636cd",
+    "url": "/ReactRecipeBook/index.html"
+  }
+];
