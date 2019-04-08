c0581324b5543797da601b38e1f6383316b58b8a
Dawiddd95 <dawlyc@wp.pl>
2019-04-08 17:12:21 +0200

    Updates

---
+++ precache-manifest.26add610f236eba37e082d5ac7dc7d1b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d53d97bb11e5a9b3b6014b0f1d6e46e",
+    "url": "/ReactRecipeBook/static/media/heart.9d53d97b.svg"
+  },
+  {
+    "revision": "7e96b7e6bbd83fa530fd",
+    "url": "/ReactRecipeBook/static/css/main.b51dc858.chunk.css"
+  },
+  {
+    "revision": "7884afb693bc74ea12e4",
+    "url": "/ReactRecipeBook/static/js/runtime~main.b22db41e.js"
+  },
+  {
+    "revision": "226ed384bf43dd7b9ea5",
+    "url": "/ReactRecipeBook/static/js/2.433f7a85.chunk.js"
+  },
+  {
+    "revision": "a9217539a623c3f3607f5145239c38cd",
+    "url": "/ReactRecipeBook/static/media/logo.a9217539.svg"
+  },
+  {
+    "revision": "b563ffb40e36ad457e1f9e6ef07c8d26",
+    "url": "/ReactRecipeBook/static/media/deleteRecipe.b563ffb4.svg"
+  },
+  {
+    "revision": "d654ce194e0660d047d6913124f8d562",
+    "url": "/ReactRecipeBook/static/media/like.d654ce19.svg"
+  },
+  {
+    "revision": "7e96b7e6bbd83fa530fd",
+    "url": "/ReactRecipeBook/static/js/main.1e254d9a.chunk.js"
+  },
+  {
+    "revision": "ea3d28876add859e465fb077f876e658",
+    "url": "/ReactRecipeBook/static/media/recipe.ea3d2887.jpg"
+  },
+  {
+    "revision": "b0f643f85319af99636b9fa665f7a26c",
+    "url": "/ReactRecipeBook/static/media/addIngredient.b0f643f8.svg"
+  },
+  {
+    "revision": "46af1e91f1800aae32ebc24ad13dbc90",
+    "url": "/ReactRecipeBook/static/media/addMethod.46af1e91.svg"
+  },
+  {
+    "revision": "e67dc1fada748153b56c13efd0706483",
+    "url": "/ReactRecipeBook/static/media/intro-img1.e67dc1fa.jpg"
+  },
+  {
+    "revision": "94d34a65cd61e846d9d0e683d966f99a",
+    "url": "/ReactRecipeBook/static/media/empty.94d34a65.png"
+  },
+  {
+    "revision": "9c27c92354f58e8f9f6069055996e224",
+    "url": "/ReactRecipeBook/static/media/DTLNobelT.9c27c923.otf"
+  },
+  {
+    "revision": "99687968f253eac7d37ddc57445d03e5",
+    "url": "/ReactRecipeBook/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ReactRecipeBook/precache-manifest.1e8571835ca9c559c8ef243385c69ae7.js"
+  "/ReactRecipeBook/precache-manifest.26add610f236eba37e082d5ac7dc7d1b.js"
 );
 
 workbox.clientsClaim();
