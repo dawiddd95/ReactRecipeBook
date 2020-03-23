5aa8583df16577b2bc4979b39e6e816d6b58931b
Dawid <dawlyc@wp.pl>
2020-03-24 00:15:54 +0100

    Add files via upload

--- src/components/HomePage/Main/StyledMain.js
+++ src/components/HomePage/Main/StyledMain.js
@@ -3,11 +3,10 @@ import backgroundImg from '../../../images/home.jpg';
 
 export const Wrapper = styled.div`
    width: 100%;
-   height: 100vh;
+   height: 100%;
    background-image: url(${backgroundImg});
    background-position: center;
    background-size: cover;
-   background-repeat: no-repeat;
 
    display: flex;
    justify-content: space-between;
